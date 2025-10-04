098bc3aef4fd507191b6ba759097f6ca210d174b
ARIES5533 <olumokomoses94@gmail.com>
2025-10-04 22:58:21 +0100

    infra script updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "my-terraform-state-bucket"
+    bucket       = "my-terraform-state-bucket5533"
     key          = "ec2-infra/terraform.tfstate"
     region       = "us-east-1"
     encrypt       = true
